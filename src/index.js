18f688c1bce2c0d7aab022f114011ce5998bc4c0
Mouad Debbar <mouad.debbar@gmail.com>
2017-09-11 20:24:18 -0700

    Some changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import importRule from './rules/no-full-import'
-
-export default {
-  rules: {
-    'no-full-import': importRule,
-  },
-  rulesConfig: {
-    'no-full-import': ['error', []],
-  },
+module.exports.rules = {
+  'no-full-import': require('./rules/no-full-import'),
 }
